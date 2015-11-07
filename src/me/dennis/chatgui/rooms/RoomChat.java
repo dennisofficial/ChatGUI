3d855a60943b511256d1aaeddaee1ba63e200bc2
Dennis Lysenko <dennislysenko@hotmail.com>
2015-11-07 18:09:46 -0500

    Removed comments, debug messages, created special char list.

--- src/me/dennis/chatgui/rooms/RoomChat.java
+++ src/me/dennis/chatgui/rooms/RoomChat.java
@@ -1,15 +1,18 @@
 package me.dennis.chatgui.rooms;
 
+import static java.awt.event.KeyEvent.*;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import me.dennis.chatgui.core.Display;
 import me.dennis.chatgui.listeners.Keyboard;
@@ -21,10 +24,35 @@ public class RoomChat extends Room {
 
 	public String nickname;
 
-	private int scroll = 0;
+	private HashMap<Integer, String[]> chars = new HashMap<Integer, String[]>();
 	private List<String> log = new ArrayList<String>();
 	private String output = "";
-	
+
+	public RoomChat() {
+		chars.put(VK_SPACE, new String[] {" ", " "});
+		chars.put(VK_COMMA, new String[] {",", "<"});
+		chars.put(VK_PERIOD, new String[] {".", ">"});
+		chars.put(VK_SLASH, new String[] {"/", "?"});
+		chars.put(VK_SEMICOLON, new String[] {";", ":"});
+		chars.put(VK_QUOTE, new String[] {"'", "\""});
+		chars.put(VK_OPEN_BRACKET, new String[] {"[", "{"});
+		chars.put(VK_CLOSE_BRACKET, new String[] {"]", "}"});
+		chars.put(VK_BACK_SLASH, new String[] {"\\", "|"});
+		chars.put(VK_MINUS, new String[]{"-", "_"});
+		chars.put(VK_EQUALS, new String[]{"=", "+"});
+		chars.put(VK_BACK_QUOTE, new String[]{"`", "~"});
+		chars.put(VK_1, new String[]{"1", "!"});
+		chars.put(VK_2, new String[]{"2", "@"});
+		chars.put(VK_3, new String[]{"3", "#"});
+		chars.put(VK_4, new String[]{"4", "$"});
+		chars.put(VK_5, new String[]{"5", "%"});
+		chars.put(VK_6, new String[]{"6", "^"});
+		chars.put(VK_7, new String[]{"7", "&"});
+		chars.put(VK_8, new String[]{"8", "*"});
+		chars.put(VK_9, new String[]{"9", "("});
+		chars.put(VK_0, new String[]{"0", ")"});
+	}
+
 	@Override
 	public void init() {
 	}
@@ -36,40 +64,52 @@ public class RoomChat extends Room {
 			String message = MessageProtocol.getMessage();
 			log.add(from + ": " + message);
 		}
-		for (int i = 0; i < KeyEvent.KEY_LAST; i++) {
-			if (Keyboard.isDirect(KeyEvent.VK_SHIFT)) {
-				if (Keyboard.isPressed(i)) {
-					if (KeyEvent.getKeyText(i).length() == 1) {
-						output += KeyEvent.getKeyText(i).toUpperCase();
+		for (int i = 0; i < KEY_LAST; i++) {
+			if (!(i >= VK_0 && i <= VK_9)) {
+				if (Keyboard.isDirect(VK_SHIFT)) {
+					if (Keyboard.isPressed(i)) {
+						if (getKeyText(i).length() == 1) {
+							output += getKeyText(i).toUpperCase();
+						}
 					}
-					otherKeyboardCheck(i);
 				}
-			}
-			else {
-				if (Keyboard.isPressed(i)) {
-					if (KeyEvent.getKeyText(i).length() == 1) {
-						output += KeyEvent.getKeyText(i).toLowerCase();
+				else {
+					if (Keyboard.isPressed(i)) {
+						if (getKeyText(i).length() == 1) {
+							output += getKeyText(i).toLowerCase();
+						}
 					}
-					otherKeyboardCheck(i);
 				}
 			}
 		}
+		otherKeyboardCheck();
 	}
 
-	public void otherKeyboardCheck(int i) { // ADD SPACES, SLASHES, ETC.
-		if (Keyboard.isPressed(KeyEvent.VK_BACK_SPACE)) {
+	public void otherKeyboardCheck() {
+		if (Keyboard.isPressed(VK_BACK_SPACE)) {
 			if (output.length() > 0) {
 				output = output.substring(0, output.length() - 1);
 			}
 		}
-		if (Keyboard.isPressed(KeyEvent.VK_ENTER)) {
+		if (Keyboard.isPressed(VK_ENTER)) {
 			if (output.length() > 0) {
 				NetworkManager.sendMessage(output);
 				output = "";
 			}
 		}
+		// Special characters
+		for (Entry<Integer, String[]> entry : chars.entrySet()) {
+			if (Keyboard.isPressed(entry.getKey())) {
+				if (Keyboard.isDirect(VK_SHIFT)) {
+					output += entry.getValue()[1];
+				}
+				else {
+					output += entry.getValue()[0];
+				}
+			}
+		}
 	}
-	
+
 	@Override
 	public void draw(Graphics2D g) {
 		g.setColor(new Color(0xBBBBBB));
@@ -89,14 +129,12 @@ public class RoomChat extends Room {
 		}
 		font = font.deriveFont(20F);
 		g.setFont(font);
-		
+
 		g.setColor(new Color(0xFFFFFF));
-		int inputNum = 0;
 		for (int i = 0; i < log.size(); i++) {
-			g.drawString(log.get(i), 10, font.getSize() + (font.getSize() * i));
-			inputNum++;
+			g.drawString(log.get(i), 10, Display.HEIGHT - font.getSize() - (font.getSize() * i));
 		}
-		g.drawString(nickname + ": " + output, 10, font.getSize() + (font.getSize() * inputNum));
+		g.drawString(nickname + ": " + output + "_", 10, Display.HEIGHT);
 	}
 
 }
